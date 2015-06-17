fb12bec0d7ee48ce5b6bdb98f370286dc9b6fc68
Roy van de Water <roy.vwater@gmail.com>
2015-06-17 10:46:44 -0700

    Add two helpers

---
+++ unload-flows.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+FAILED_UNITS=$(fleetctl list-units | grep octo- | grep -v octo-master | grep failed | awk '{print $1}')
+
+for UNIT in $FAILED_UNITS; do
+  echo $UNIT
+  fleetctl unload $UNIT
+done
